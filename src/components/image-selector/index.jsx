4d1316434de52d22bfbda5c98dc856a6b344532b
Matias Coronel <97346804+MatiasCoronel1312@users.noreply.github.com>
2022-12-26 23:56:06 -0300

    agregue utilizacion de camara

---
+++ src/components/image-selector/index.jsx
@@ -0,0 +1,62 @@
+import * as ImagePicker from "expo-image-picker";
+import React, { useState } from "react";
+import { View, Text, Image, Alert, Button } from "react-native";
+
+import { styles } from "./styles";
+import  colors  from "../../utils/colors";
+
+const ImageSelector = ({onImagePicker}) => {
+    const [pickedUrl, setPickedUrl] = useState(null);
+
+    const onHandleTakeImage = async () => {
+        const isCameraPermissions = await verifyPermissions();
+        if(!isCameraPermissions) return;
+
+        const image = await ImagePicker.launchCameraAsync({
+            allowsEditing: true,
+            aspect: [16,9],
+            quality: 0.7,
+            
+        });
+
+        //console.warn('image', image);
+
+        setPickedUrl(image.uri);
+        onImagePicker(image.uri);
+    };
+
+
+    const verifyPermissions = async () => {
+        const {status} = await ImagePicker.requestCameraPermissionsAsync();
+        if (status!== 'granted') {
+            Alert.alert(
+                'Permisos insuficientes',
+                'Necesita dar permisos para usar la camara', [{ text: 'ok'}]
+            );
+
+            return false;
+        }
+        return true;
+    }
+    return (
+        <View style={styles.container}>
+            <View style={styles.preview}>
+                {!pickedUrl? (
+                    <Text style={styles.title}>No hay imagen seleccionada</Text>
+                ): (
+                    <Image style={styles.image} source={{uri: pickedUrl}}/>
+                )}
+            </View>
+            <View style={styles.containerButton}>
+                <Button
+                    title='Tomar Foto'
+                    color={colors.primary}
+                    onPress={onHandleTakeImage}
+                    
+                />
+            </View>
+        </View>
+    )
+};
+
+export default ImageSelector;
--- src/screens/new-place/index.jsx
+++ src/screens/new-place/index.jsx
@@ -2,14 +2,38 @@ import { View, Text, ScrollView, TextInput, Button } from "react-native";
 
 import { styles } from "./styles";
 import colors from "../../utils/colors";
+import { useDispatch } from "react-redux";
+import React ,{ useState } from "react";
+import {  savePlace } from "../../store/place.slice";
+import { ImageSelector } from "../../components";
 
 const NewPlace = ({ navigation }) => {
+  const dispatch = useDispatch();
+  const [ title, setTitle] = useState('');
+  const [image, setImage] = useState('');
+  
+
+  const onHandleSubmit = () => {
+    dispatch(savePlace({title, image}));
+    navigation.navigate('Places');
+  }
+const onHandleChange = (text) => {
+  setTitle(text);
+  
+};
+
+const onImagePicker = (uri) => {
+  setImage(uri);
+  
+};
+
   return (
     <ScrollView style={styles.container}>
       <View style={styles.content}>
         <Text style={styles.title}>Titulo</Text>
-        <TextInput style={styles.input} placeholder='Escribe la direccion' />
-        <Button color={colors.primary} title='Guardar Direccion' onPress={() => {null}}/>
+        <TextInput style={styles.input} placeholder='Escribe la direccion' onChangeText={onHandleChange}/>
+        <ImageSelector onImagePicker={onImagePicker}/>
+        <Button color={colors.primary} title='Guardar Direccion' onPress={onHandleSubmit}/>
       </View>
     </ScrollView>
   );
